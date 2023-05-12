1b1ea6891f292849d656e76e45a0e2afca493c8b
medinfo2022 <benfarhat.mohamed9@gmail.com>
2023-05-12 10:46:43 +0100

    Integrate signin-signup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -7,11 +8,26 @@ import { FruitsModule } from './fruits/fruits.module';
 import { HttpClientModule } from '@angular/common/http';
 import { CategoriesModule } from './categories/categories.module';
 
+import { BoardAdminComponent } from './board-admin/board-admin.component';
+import { BoardModeratorComponent } from './board-moderator/board-moderator.component';
+import { BoardUserComponent } from './board-user/board-user.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { ProfileComponent } from './profile/profile.component';
+import { RegisterComponent } from './register/register.component';
+
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BoardAdminComponent,
+    BoardModeratorComponent,
+    BoardUserComponent,
+    HomeComponent,
+    LoginComponent,
+    ProfileComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
@@ -19,6 +35,7 @@ import { CategoriesModule } from './categories/categories.module';
     FruitsModule,
     HttpClientModule,
     CategoriesModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
